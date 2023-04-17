6c224732884005c2316c05d5bc1bd3453cf89738
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2023-04-17 21:50:00 +0100

    first commit

---
+++ private/core/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('ROOT', 'http://localhost/schoolbox/public');
+define('ASSETS', 'http://localhost/schoolbox/public/assets');
+
+define('DBNAME', 'schoolbox_db');
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBDRIVER', 'mysql');
+
